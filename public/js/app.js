06e542266fba2546e6161259ce434634a2853bc8
hanmiton <hanmilton_12@outlook.com>
2016-06-14 21:42:59 -0500

    Añadiendo ngRoute y rutas vasicas para cargar los Ingenieros

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,21 @@
 (function () {
   var app = angular.module('ingedex', [
+    'ngRoute',
     'ingedex.controllers',
     'ingedex.directives',
     'ingedex.filters'
 
     ]);
+
+    app.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/ingeniero.html',
+        controller: 'IngenieroController',
+        controllerAs: 'ingCtrl'
+      });
+
+  }]);
 
 })();
