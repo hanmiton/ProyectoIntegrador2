8d1553c1c607cf1438faed19cbf4557200c92c4c
hanmiton <hanmilton_12@outlook.com>
2016-06-15 16:08:01 -0500

    Implementando busqueda

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,21 +3,37 @@
   angular.module('ingedex.controllers', [])
     .controller('IngedexController', ['$rootScope', '$scope', '$routeParams', 'ingenieroService', function ($rootScope, $scope, $routeParams, ingenieroService) {
       var type = $routeParams.type;
+       var ingenieros = [];
+
       $rootScope.title = "";
 
       if (type) {
         $scope.type = type;
 
         ingenieroService.byType(type).then(function (data) {
-          $scope.ingenieros = data
+          $scope.ingenieros = ingenieros =data;
           $scope.groupped = partition(data, 4);
         });
       } else {
         ingenieroService.all().then(function (data) {
-          $scope.ingenieros = data;
+          $scope.ingenieros = ingenieros =data;
           $scope.groupped = partition(data, 4);
         });
       }
+
+      $scope.search = function () {
+        var result = ingenieros;
+        if ($scope.searchTerm) {
+          result = ingenieros.filter(function (ingeniero) {
+            var name = ingeniero && ingeniero.name || "";
+
+            return name.toLowerCase().indexOf($scope.searchTerm.toLowerCase()) !== -1;
+          });
+        }
+
+        $scope.ingenieros = result;
+        $scope.groupped = partition(result, 4);
+      };
 
       function partition(data, n) {
         return _.chain(data).groupBy(function (element, index) {
